7b3874848e849a4426c084c8dbd1b8d6693d7cd6
Evgen <evgen.hordubei@gmail.com>
2026-08-15 17:18:02 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,119 @@
+// let a = 31
+// console.log(a)
+
+// const number = Number('51');
+// console.log(Number.isNaN(number));
+
+// const string = Number('dasaasa');
+// console.log(Number.isNaN(string));
+
+// console.log(Number.parseInt('746fhs84di'));
+// console.log(Number.parseInt('fjdif.,f93df,,'));
+
+// let str = 'hello world';
+// let name = 'Софійко';
+// let str2 = `Привіт, ${name}!`;
+// console.log(str2);
+
+// const massege = 'javascript ' + 'is' + ' awesome';
+// console.log(massege);
+
+// const user = {}
+// user.name = 'Софійко';
+// user.age = 31;
+// console.log(user);
+
+// const user = {
+//     name: 'Софійко',
+//     age: 31,
+//     isAdmin: true,
+// }
+
+// for (const key in user) {
+//     console.log(key);
+//     console.log(user[key]);
+// }
+
+// const test = confirm('Are you sure?');
+// console.log(test);
+
+// value = false;
+// console.log(typeof value);
+// const NewValue = String(value);
+// console.log(typeof NewValue);
+
+// value = "123";
+// console.log(typeof value);
+// const NewValue = Number(value);
+// console.log(typeof NewValue);
+
+// console.log(1 + 2);
+// console.log(1 - 2);
+// console.log(1 * 2);
+// console.log(1 / 2);
+
+// alert('Hello, world!');
+
+// switch ('Oleg') {
+//     case 'Nastya':
+//         console.log('true');
+//         break;
+//     case 'Roma':
+//         console.log('true');
+//         break;
+//     case 'Vika':
+//         console.log('true');
+//         break;
+// }
+
+// let leng = 'ru';
+
+// if (leng === 'ua') {
+//     console.log('мова');
+// } else if (leng === 'en') {
+//     console.log('language');
+// } else {
+//     console.log('error');
+// }
+
+// if (2 + 3 === 4) {
+//     console.log('true');
+// } else {
+//     console.log('false');
+// }
+
+
+// const min = Number(prompt('Введіть хвилини'));
+
+// if (min >= 0 && min <= 15) {
+//     console.log('Перша чверть');
+// } else if (min >= 16 && min <= 30) {
+//     console.log('Друга чверть');
+// } else if (min >= 31 && min <= 45) {
+//     console.log('Третя чверть');
+// } else if (min >= 46 && min <= 59) {
+//     console.log('Четверта чверть');
+// } else {
+//     console.log('Помилка');
+// }
+
+
+const value = prompt('Введіть назву товару').toLowerCase();
+let cost;
+
+switch (value) {
+    case 'риба':
+        cost = 100;
+        alert(`Вартість товару ${value} становить ${cost} грн.`);
+        break;
+    case 'молоко':
+        cost = 50;
+        alert(`Вартість товару ${value} становить ${cost} грн.`);
+        break;
+    case 'хліб':
+        cost = 20;
+        alert(`Вартість товару ${value} становить ${cost} грн.`);
+        break;
+    default:
+        alert(`Товар ${value} не знайдено`);
+}
